f000381bcdd151dab759e1e7a06a7b6bf3f335ef
reyz5 <149589918+reyz5@users.noreply.github.com>
2024-12-28 02:29:55 +0100

    Update a.php

--- a.php
+++ a.php
@@ -1,1 +1,9 @@
-
+{
+  "ip" : "2a02:aee3f:aaaa:c101:2993:rr3c:dc87:0000, 000.000.000.000",
+  "key" : "KYOJIN LKING",
+  "agent" : "Free%20Fire\/2019117860 CFNetwork\/1399 Darwin\/22.1.0",
+  "time" : "2024-12-27 18:26:52",
+  "status" : "heheheboy",
+  "UUID" : "2DE30CF2-D125-4E60-8735-FC05852A89B0",
+  "amount" : "99999999999 KYOJIN"
+}
